deef94026b7bbed6968d07a29911865ebcf149e0
nilyukov <nilyukov@gmail.com>
2020-09-16 16:51:23 +0300

    Create Currency Repository

--- app/Repositories/Admin/CurrencyRepository.php
+++ app/Repositories/Admin/CurrencyRepository.php
@@ -4,7 +4,47 @@
 namespace App\Repositories\Admin;
 
 
-class CurrencyRepository
-{
+use App\Models\Admin\Currency;
+use App\Repositories\CoreRepository;
 
+class CurrencyRepository extends CoreRepository
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function getModelClass()
+    {
+        return Currency::class;
+    }
+
+    public function getAllCurrency()
+    {
+        $curr = $this->startConditions()::all();
+        return $curr;
+    }
+
+    public function getInfoProduct($id){
+        $product = $this->startConditions()
+            ->find($id);
+        return $product;
+    }
+
+    public function switchBaseCurr()
+    {
+        $id = Currency::where('base',2)->pluck('id')->toArray();
+        $id = $id[0];
+        $new = Currency::find($id);
+        $new->base = '0';
+        $new->save();
+    }
+
+    public function deleteCurrency($id)
+    {
+        $delete = $this->startConditions()
+            ->where('id', $id)
+            ->forceDelete();
+        return $delete;
+    }
 }
